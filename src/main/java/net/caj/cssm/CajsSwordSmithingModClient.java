abc0f9f022e2a4bd10be7692ef21ef2ba10ff29f
Panther0706 <colejordan1313@gmail.com>
2024-11-30 16:21:11 -0500

    Initial commit

---
+++ src/main/java/net/caj/cssm/CajsSwordSmithingModClient.java
@@ -0,0 +1,10 @@
+package net.caj.cssm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CajsSwordSmithingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
